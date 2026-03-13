9bdcd518c05f752408b46eb0f91df2b8660acaca
Jonialen <jonathanalej09@gmail.com>
2026-03-12 23:49:10 -0600

    refactor: standardize Spanish variable names to English
    - Rename capas -> layers, fila -> row, columnas -> columns
    - Rename cofre_idle -> chest_idle, cofre_hover -> chest_hover
    - Rename columnasCofre -> chestColumns

--- src/game/animations/AnimationSetup.ts
+++ src/game/animations/AnimationSetup.ts
@@ -3,13 +3,13 @@ import { createRowAnimation } from '../utils/createRowAnimation';
 export function setupAnimations(scene: Phaser.Scene) {
   const texCofre = scene.textures.get('cofre')?.getSourceImage();
   if (texCofre) {
-    const columnasCofre = texCofre.width / 48;
-    createRowAnimation(scene, 'cofre_hover', 'cofre', 0, columnasCofre, 6, 0);
+    const chestColumns = texCofre.width / 48;
+    createRowAnimation(scene, 'chest_hover', 'cofre', 0, chestColumns, 6, 0);
   }
 
   const texCofre1 = scene.textures.get('cofre1')?.getSourceImage();
   if (texCofre1) {
-    const columnasCofre1 = texCofre1.width / 32;
-    createRowAnimation(scene, 'cofre_idle', 'cofre1', 0, columnasCofre1, 2);
+    const chestColumns1 = texCofre1.width / 32;
+    createRowAnimation(scene, 'chest_idle', 'cofre1', 0, chestColumns1, 2);
   }
 }
